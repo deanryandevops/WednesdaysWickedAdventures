de0d865845e58827d033da83f93ea1aecb904cc4
dean.ryan <L00188387@atu.ie>
2025-03-25 20:54:13 +0000

    SCRUM-67 / Infrastructure-session
    Built the terraform Structure with Joseph
    Added Choice Parameter of Deploy and Destroy

---
+++ Jenkin-Pipelines/Infrastructure-Pipeline.groovy
@@ -0,0 +1,79 @@
+pipeline 
+{
+    agent any
+    
+    parameters{
+        choice(name: 'Action', choices: ['deploy', 'destroy'], description: 'Select Terraform Action to run')
+    }
+    
+    environment
+    {
+        AWS_ACCESS_KEY = credentials('aws-access-key')
+        AWS_SECRET_KEY = credentials('aws-secret-key')
+        SLACK_CHANNEL = "#terraform-infrastructure"
+    }
+    
+    stages
+    {
+        stage('Clone Repository')
+        {
+            steps 
+            {
+                git branch: 'production', url: 'https://github.com/deanryandevops/WednesdaysWickedAdventures.git'
+            }
+        }
+        
+        stage('List Directory')
+        {
+            steps
+            {
+                powershell ''' ls '''
+            }
+        }
+        
+        stage('Change Directory to Terraform Folder')
+        {
+            steps {
+                
+                dir('terraform') 
+                {
+                    script
+                    {
+                    powershell ''' terraform init '''
+                    powershell ''' terraform validate '''
+                    
+                    if(params.Action == 'deploy')
+                    {
+                        powershell ''' terraform plan -out=tfplan '''
+                        powershell ''' terraform apply tfplan '''
+                    }
+                    else
+                    {
+                        powershell ''' terraform destroy --auto-approve '''
+                    }
+                    }
+                }
+            }
+        }
+        
+        stage('List Terraform Directory')
+        {
+            steps
+            {
+                powershell ''' ls '''
+            }
+        }
+    }
+    post 
+    {
+        success{
+            echo 'Terraform deployment successful'
+            slackSend channel: "{env.SLACK_CHANNEL}", color:"good", tokenCredentialId: 'slack-token', message: 'Deployment succeeded'
+        }
+        failure
+        {
+            echo 'Terraform deployment failed'
+            slackSend channel: "{env.SLACK_CHANNEL}", color:"danger", tokenCredentialId: 'slack-token', message: 'Deployment failed'
+        }
+    }
+}
